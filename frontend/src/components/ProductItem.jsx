1441d14ee5d7fc9eed64752b2b54345bdd5b31e1
Tousif Tamboli <tousiftamboli3@gmail.com>
2025-02-26 14:50:00 +0530

    tried to add border

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -6,19 +6,25 @@ const ProductItem = ({ id, image, name, price }) => {
   const { currency } = useContext(ShopContext);
 
   return (
-    <Link className="text-gray-700 cursor-pointer" to={`/product/${id}`}>
-      <div className="overflow-hidden">
-        <img
-          className="hover:scale-110 transition ease-in-out"
-          src={image[0]}
-          alt={name}
-        />
+    <Link
+      className="text-gray-700 cursor-pointer"
+      to={`/product/${id}`}
+    >
+      <div className="border rounded-lg p-2 hover:shadow-md transition">
+        <div className="overflow-hidden border-2 border-black-100 ">
+        <h1>hello</h1>
+          <img
+            className="hover:scale-110 transition ease-in-out"
+            src={image[0]}
+            alt={name}
+          />
+        </div>
+        <p className="pt-3 pb-1 text-sm">{name}</p>
+        <p className="text-sm font-medium">
+          {currency}
+          {price}
+        </p>
       </div>
-      <p className="pt-3 pb-1 text-sm">{name}</p>
-      <p className="text-sm font-medium">
-        {currency}
-        {price}
-      </p>
     </Link>
   );
 };
